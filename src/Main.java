f8e79ff5874008d79045485833b5e6d61684e8cc
DimaAn88-2 <drearywizard@rambler.ru>
2022-11-03 23:42:01 +0300

    now

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int x =5;
+        int v=7;
+        int d = x+v;
+        System.out.println(d);
     }
 }
